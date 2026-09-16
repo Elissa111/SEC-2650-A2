b3c010fff9d8afeacab33b47538b647409ec4d61
Elissa111 <promitzhayashielissa@gmail.com>
2026-09-16 22:37:53 +1000

    Adding secret.php

---
+++ A2-Q1/secrets.php
@@ -0,0 +1,7 @@
+<?php
+// Keep your Stripe API key protected by including it as an environment variable
+// or in a private script that does not publicly expose the source code.
+
+// This is your test secret API key.
+// Don't put any keys in code. See https://docs.stripe.com/keys-best-practices.
+$stripeSecretKey = 'sk_test_51UEhXtCtSpSPgIChWU3CXjNc8ac0rTeJS0nzXmMkecu0TX3V8FIauz1z0FfZQ3qJDuVeEFMxYNNKV1mpGULMsBWP00vCovKVCU';
